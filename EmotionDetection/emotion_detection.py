0942348fe9ead490982d46a2bfad8b207f45ac80
Codevals <koltemb@gmail.com>
2025-02-23 04:32:23 -0500

    stage 7

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,32 @@
+import requests, json
+
+def emotion_detector(text_to_analyze):
+    # url of the API emotion analysis service
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    # Custom header specifying the model ID for emotion analysis service
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    # Constructing the request payload in the expected format
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    # Sending the POST request to the sentiment analysis API
+    response = requests.post(url, json = myobj, headers=header)
+    # Parsing the JSON response from the API
+    formatted_response = json.loads(response.text)
+    formatted_response = formatted_response['emotionPredictions'][0]['emotion']
+    #extracting emotion anger, fear, joy, sadness, and dominant_emotion from the response
+    data = {
+        'anger': formatted_response['anger'],
+        'disgust': formatted_response['disgust'],
+        'fear': formatted_response['fear'],
+        'joy': formatted_response['joy'],
+        'sadness': formatted_response['sadness']
+    }
+    dominant_emotion = max(data, key=data.get)
+    #return response json from API
+    return {
+        'anger': formatted_response['anger'],
+        'disgust': formatted_response['disgust'],
+        'fear': formatted_response['fear'],
+        'joy': formatted_response['joy'],
+        'sadness': formatted_response['sadness'],
+        'dominant_emotion': dominant_emotion
+    }
---
+++ server.py
@@ -0,0 +1,33 @@
+# Import Flask, render_template, request from the flask framework package 
+from flask import Flask, render_template, request
+# Import the emotion_detector function from the package created
+from EmotionDetection.emotion_detection import emotion_detector
+# Initiate the flask app
+app = Flask("Emotion Analyzer")
+@app.route("/emotionDetector")
+def sent_analyzer():
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+    # Pass the text to the emotion_detector function and store the response
+    response = emotion_detector(text_to_analyze)
+    # Extract the anger, digust, fear, joy, sadness and dominant_emotion from the response
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+    return f"'Anger': {anger}, 'disgust:' {disgust}, 'fear': {fear}, 'joy': {joy}, 'sadness': {sadness}, The dominant emotion is {dominant_emotion} "
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    # To be completed
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    # This functions executes the flask app and deploys it on localhost:5000'''
+    # To be completed
+    app.run(host="0.0.0.0", port=5000)
